a82a0038b5f368ca46f9c58261ec3b69079e8092
nur-zilya <89769554+nur-zilya@users.noreply.github.com>
2021-11-10 17:34:27 +0300

    Add files via upload

---
+++ clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf ~/Library/Application\ Support/Slack/Cache/*
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+rm -rf ~/Library/Application\ Support/discord/Cache/*
+rm -rf ~/Library/Application\ Support/Code/Cache/*
+rm -rf /Users/hfast/Library/Application\ Support/Code/CachedData/*
+rm -rf /Users/hfast/Library/Application\ Support/Code/logs/*
+rm -rf /Users/hfast/Library/Application\ Support/Code/User/*
+rm -rf ~/Library/Caches/*
+rm -rf ~/Library/*42_cache*
+rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Service Worker/CacheStorage/*
